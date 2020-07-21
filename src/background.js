11cd14bc5406b04f20c319cc845c9db4fb9ab16c
Arun R <arun@ftltech.io>
2020-07-22 02:35:44 +0530

    Improv: Add app icon

--- src/background.js
+++ src/background.js
@@ -1,8 +1,10 @@
 "use strict";
+/* global __static */
 
 import { app, protocol, BrowserWindow } from "electron";
 import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
 import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
+import path from "path";
 const isDevelopment = process.env.NODE_ENV !== "production";
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -19,6 +21,7 @@ function createWindow() {
   win = new BrowserWindow({
     width: 1000,
     height: 600,
+    icon: path.join(__static, "img/icons/padma.png"),
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
