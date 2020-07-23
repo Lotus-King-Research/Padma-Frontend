f05544da00246eae01b4581ae1c03e89e2bd19d2
Arun R <arun@ftltech.io>
2020-07-23 23:54:59 +0530

    Improv: Replace dynamic google font load with npm module

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import { BootstrapVue } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
+import(/* webpackPreload: true */ "typeface-jomolhari/index.css");
+
 import Toasted from "vue-toasted";
 Vue.use(Toasted);
 
