58eeeaa2d5addb1109ef7b0aac327d705169839a
Mikko Kotila <mikko@pop-os.localdomain>
2020-07-29 18:38:31 +0300

    improved spacing in dictionary_lookup

--- src/components/Pages/DictionaryLookup.vue
+++ src/components/Pages/DictionaryLookup.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="dictionarylookup">
-    <div class="container-fluid pt-3 pb-2 pl-4">
+    <div class="container-fluid pb-2 pl-4">
       <font class="tibetan-text-reader">{{ searchQuery }}</font>
     </div>
     <div class="container-fluid">
